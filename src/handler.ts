33fd9656ef2865ccdf3c873810d6d757863ed980
PhilLavoie <philippe.lavoie-mongrain@breather.com>
2024-02-08 19:21:34 -0500

    NCC over webpack
    - Leverage NCC for source mapping
    - Update action.yml to point to new dist script

--- src/handler.ts
+++ src/handler.ts
@@ -1,4 +1,4 @@
-import core from "@actions/core";
+import * as core from "@actions/core";
 import { HandlerOutputs, HandlerParams } from "./types.js";
 
 export function handler(params: HandlerParams): Promise<HandlerOutputs> {
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { getInputs, runActionHandler } from "@infra-blocks/github-actions";
-import { install } from "source-map-support";
 import { handler } from "./handler.js";
 import { parseInputs } from "./inputs.js";
 
 runActionHandler(() => {
-  install();
   return handler(parseInputs(getInputs("example-input")));
 });
